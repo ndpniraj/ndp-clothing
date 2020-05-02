474180371680305a6d7ce6d676553d2cec34e93e
ndpniraj <ndpniraj@gmail.com>
2020-05-02 15:58:35 +0530

    implimenting sign in method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../../firebase/firebase.utils';
 class SignIn extends React.Component {
 	constructor(props) {
 		super(props);
@@ -12,15 +12,19 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = e => {
+	handleSubmit = async e => {
 		e.preventDefault();
-		this.setState({ email: '', password: '' });
-		console.log(this.state);
+		const { email, password } = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: '', password: '' });
+		} catch (error) {
+			console.log('error while sign in', error.message);
+		}
 	};
 
 	handleChange = e => {
 		const { name, value } = e.target;
-		console.log(name, value);
 		this.setState({ [name]: value });
 	};
 
